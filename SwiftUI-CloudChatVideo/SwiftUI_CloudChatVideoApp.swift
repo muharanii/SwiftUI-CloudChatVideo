a94d3b614fe6c97354eed48eef7722686f4cb6ac
muharaniira <muharaniira@gmail.com>
2024-08-30 08:40:47 +0700

    Initial Commit

---
+++ SwiftUI-CloudChatVideo/SwiftUI_CloudChatVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CloudChatVideoApp.swift
+//  SwiftUI-CloudChatVideo
+//
+//  Created by Ira Muharani on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CloudChatVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
